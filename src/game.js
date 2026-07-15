c50c77cc59a80023b75fe3671b385e01402ab7af
Forkentiney <jseverud@proton.me>
2026-07-15 10:00:38 -0500

    Added rudimentary ship factory

--- src/game.js
+++ src/game.js
@@ -1,4 +1,17 @@
-function ship() {};
+function ship(length) {
+	const defenses = length;
+	let damages = 0;
+	const hit = () => {
+		damages++;
+	};
+	const isSunk = () => {
+		if (defenses > damages) {
+			return false;
+		} else {
+			return true;
+		};
+	};
+};
 
 function gameboard() {};
 
