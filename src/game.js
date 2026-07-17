a3951bf14d31b29f056ab57481cb6056b9496c01
Forkentiney <jseverud@proton.me>
2026-07-16 22:09:10 -0500

    Wrote some ideas for how to approach ships

--- src/game.js
+++ src/game.js
@@ -19,7 +19,38 @@ function ship(length) {
 	};
 };
 
-function gameboard() {};
+function gameboard() {
+	const board = [ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+									[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+									[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+									[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+									[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+									[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+									[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+									[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+									[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+									[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ];
+	const placeShip = (coordinate, orientation) => {
+		// Take the starting coordinate and
+		const x = coordinate[0];
+		const y = coordinate[1];
+		// Make each ship have n number of coords stored in an array:
+		// shipCoords = []. Check that shipCoords does not contain any other
+		// taken coords which are stored in a main gameboard array. If they
+		// are in the main array, return an error. If they are not, push the
+		// each ship coord into the main array. When the game is played, if
+		// a coord is not in the main array, register a miss. There's no 
+		// need for the board array with this method.
+		for (let i = 0; i < ship.length; i++) {
+			if (board[x][y] === 1) {
+
+			}
+		}
+		// the orientation of a ship.
+	  // Check that the space is not occupied
+		// and place the ship.
+	};
+};
 
 function player() {};
 
