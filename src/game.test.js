681850e69060d6b56b0e28f125abd3af97c15234
Forkentiney <jseverud@proton.me>
2026-07-21 13:07:28 -0500

    Wrote test to check vertical ships and validity

--- src/game.test.js
+++ src/game.test.js
@@ -9,8 +9,20 @@ test('Created Patrol Boat', () => {
 	expect(ship(5).name).toBe("Patrol Boat");
 });
 
-test('Place ship on board', () => {
+test('Place ship horizontally on board', () => {
 	const board = gameBoard();
 	board.placeShip(ship(1), [0, 0], "horizontal");
 	expect(board.shipPlacements[0].placement).toStrictEqual([ [0, 0], [1, 0], [2, 0], [3, 0], [4, 0] ]);
+});
+
+test('Place ship vertically on board', () => {
+	const board = gameBoard();
+	board.placeShip(ship(2), [0, 3], "vertical");
+	expect(board.shipPlacements[0].placement).toStrictEqual([ [0, 3], [0, 2], [0, 1], [0, 0] ]);
+});
+
+test('Ships cannot overlap', () => {
+	const board = gameBoard();
+	board.placeShip(ship(1), [0, 0], "horizontal");
+	expect(board.placeShip(ship(2), [0, 3], "vertical")).toBe("Invalid location");
 });
