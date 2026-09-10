de287d98adea349cfd090ad19f3345693177cc82
Forkentiney <jseverud@proton.me>
2026-09-10 11:42:19 -0400

    Added icon switches on clicks
    Notably the icons shake when user attempts to click a spot they have
    already chosen

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import {
 	ship,
 	gameBoard,
 } from './game.js';
+import red from "./static/icons/circle-fill-red.svg";
+import white from "./static/icons/circle-fill-white.svg";
 
 const playerBoard = document.querySelector("#player");
 const compBoard = document.querySelector("#computer");
@@ -12,15 +14,38 @@ const cells = document.querySelectorAll(".cell");
 const computerBoard = gameBoard();
 
 cells.forEach(cell => {
-	cell.addEventListener("click", () => checkHit(computerBoard, cell.id));
+	cell.addEventListener("click", () => checkHit(computerBoard, cell));
 });
 
 
 computerBoard.placeShip(ship(2), [0, 2], "horizontal");
 
 function checkHit(board, spot) {
-	const coord = [parseInt(spot.charAt(0)), parseInt(spot.charAt(1))];
+	const coord = [parseInt(spot.id.charAt(0)), parseInt(spot.id.charAt(1))];
 	const result = board.receiveAttack(coord);
 	console.log(result);
+	if (result === "You hit!") {
+		updateCell(spot, "red");
+	} else if (result === "You missed.") {
+		updateCell(spot, "white");
+	} else {
+		updateCell(spot, "shake");
+	};
 	return result;
+};
+
+function updateCell(cell, type) {
+	const cellImg = cell.firstElementChild;
+	if (type === "red") {
+		cellImg.src = red;
+		cellImg.alt = "x";
+	} else if (type === "white") {
+		cellImg.src = white;
+		cellImg.alt = "-";
+	} else {
+		cellImg.classList.add("shake");
+		cellImg.addEventListener("animationend", () => {
+			cellImg.classList.remove("shake");
+		}, { once: true });
+	};
 };
