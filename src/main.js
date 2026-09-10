a15ec13b7620e6adf4f2bfbdd1f1cae9e1de6ac1
Forkentiney <jseverud@proton.me>
2026-09-10 10:41:05 -0400

    Started writing function for attacking on board
    I don't think the coords are translated correctly right now, but at the
    moment I am just happy that clicking on a spot on the board results in
    either a hit or a miss which is consistent and reproducable.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,25 @@ import './style.css';
 import {
 	player,
 	ship,
+	gameBoard,
 } from './game.js';
+
+const playerBoard = document.querySelector("#player");
+const compBoard = document.querySelector("#computer");
+
+const cells = document.querySelectorAll(".cell");
+const computerBoard = gameBoard();
+
+cells.forEach(cell => {
+	cell.addEventListener("click", () => checkHit(computerBoard, cell.id));
+});
+
+
+computerBoard.placeShip(ship(2), [0, 2], "horizontal");
+
+function checkHit(board, spot) {
+	const coord = [parseInt(spot.charAt(0)), parseInt(spot.charAt(1))];
+	const result = board.receiveAttack(coord);
+	console.log(result);
+	return result;
+};
