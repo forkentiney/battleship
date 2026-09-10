59d4ea47dd92dca2dd7718fea293a5fe411ac0c8
Forkentiney <jseverud@proton.me>
2026-09-10 13:16:54 -0400

    Removed ability to attack own board.

--- src/main.js
+++ src/main.js
@@ -10,18 +10,26 @@ import white from "./static/icons/circle-fill-white.svg";
 const playerBoard = document.querySelector("#player");
 const compBoard = document.querySelector("#computer");
 
-const cells = document.querySelectorAll(".cell");
+const playerCells = document.querySelectorAll("#player > *");
+const compCells = document.querySelectorAll("#computer > *");
+
 const computerBoard = gameBoard();
 
-cells.forEach(cell => {
-	cell.addEventListener("click", () => checkHit(computerBoard, cell));
+playerCells.forEach(cell => {
+	cell.addEventListener("click", () => {
+		updateCell(cell, "shake");
+		console.log("You cannot fire into your own waters");
+	});
 });
 
+compCells.forEach(cell => {
+	cell.addEventListener("click", () => checkHit(computerBoard, cell));
+});
 
 computerBoard.placeShip(ship(2), [0, 2], "horizontal");
 
 function checkHit(board, spot) {
-	const coord = [parseInt(spot.id.charAt(0)), parseInt(spot.id.charAt(1))];
+	const coord = [parseInt(spot.id.charAt(1)), parseInt(spot.id.charAt(2))];
 	const result = board.receiveAttack(coord);
 	console.log(result);
 	if (result === "You hit!") {
